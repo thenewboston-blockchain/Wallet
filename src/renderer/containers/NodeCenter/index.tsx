ef6dc38b8d3eddae23a9e20e33e546b43b775184
angle943 <justinkim943@gmail.com>
2021-09-25 23:58:22 -0500

    don't destructure node data

--- src/renderer/containers/NodeCenter/index.tsx
+++ src/renderer/containers/NodeCenter/index.tsx
@@ -10,23 +10,23 @@ import NodeCenterHeader from './NodeCenterHeader';
 import * as S from './Styles';
 
 const transformNodeData = (nodeData: NodeData[]) => {
-  return nodeData.map(({ipAddress, isActive, isPv, networkId, rank, totalBoosts, txFeeRate}) => ({
-    key: networkId,
-    [NodeCenterTableKeys.rank]: rank,
-    [NodeCenterTableKeys.isPv]: isPv ? <S.PvBadge>PV</S.PvBadge> : '',
+  return nodeData.map((node) => ({
+    key: node.networkId,
+    [NodeCenterTableKeys.rank]: node.rank,
+    [NodeCenterTableKeys.isPv]: node.isPv ? <S.PvBadge>PV</S.PvBadge> : '',
     [NodeCenterTableKeys.status]: (
       <S.StatusWrapper>
-        <S.StatusBadge status={isActive ? StatusBadgeType.active : StatusBadgeType.inactive} />
-        {isActive ? 'Active' : 'Inactive'}
+        <S.StatusBadge status={node.isActive ? StatusBadgeType.active : StatusBadgeType.inactive} />
+        {node.isActive ? 'Active' : 'Inactive'}
       </S.StatusWrapper>
     ),
-    [NodeCenterTableKeys.networkId]: truncateLongText(networkId),
-    [NodeCenterTableKeys.ipAddress]: ipAddress,
-    [NodeCenterTableKeys.txFeeRate]: txFeeRate,
+    [NodeCenterTableKeys.networkId]: truncateLongText(node.networkId),
+    [NodeCenterTableKeys.ipAddress]: node.ipAddress,
+    [NodeCenterTableKeys.txFeeRate]: node.txFeeRate,
     [NodeCenterTableKeys.totalBoosts]: (
       <S.TotalBoostsContainer>
-        <S.ProgressBar progress={totalBoosts} total={300000} />
-        {totalBoosts.toLocaleString()}
+        <S.ProgressBar progress={node.totalBoosts} total={300000} />
+        {node.totalBoosts.toLocaleString()}
       </S.TotalBoostsContainer>
     ),
     [NodeCenterTableKeys.boostAction]: (

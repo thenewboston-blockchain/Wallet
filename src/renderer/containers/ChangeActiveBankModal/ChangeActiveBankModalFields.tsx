d92d21c51da5b54bfec297f4ad18ba172eec3a7a
angle943 <justinkim943@gmail.com>
2021-09-30 22:02:43 -0500

    change active bank

--- src/renderer/containers/ChangeActiveBankModal/ChangeActiveBankModalFields.tsx
+++ src/renderer/containers/ChangeActiveBankModal/ChangeActiveBankModalFields.tsx
@@ -1,22 +1,23 @@
-import React, {FC} from 'react';
+import React from 'react';
 import {useFormContext} from '@renderer/hooks';
 import {FormInput, FormSelect} from '@renderer/components/FormComponents';
-import {InputOption} from '@renderer/types';
+import {InputOption, SFC} from '@renderer/types';
+import * as S from './Styles';
 
 const protocolOptions: InputOption[] = [{value: 'http'}, {value: 'https'}];
 
-const ChangeActiveBankModalFields: FC = () => {
+const ChangeActiveBankModalFields: SFC = ({className}) => {
   const {errors} = useFormContext();
   const addressError = errors.form;
 
   return (
-    <>
-      {addressError ? <span className="ChangeActiveBankModal__form-error">{addressError}</span> : null}
+    <S.Container className={className}>
+      {addressError ? <S.ErrorMessage>{addressError}</S.ErrorMessage> : null}
       <FormSelect focused label="Protocol" name="protocol" options={protocolOptions} required searchable={false} />
       <FormInput label="IP Address" name="ipAddress" required />
       <FormInput label="Port" name="port" type="number" required />
       <FormInput label="Nickname" name="nickname" />
-    </>
+    </S.Container>
   );
 };
 

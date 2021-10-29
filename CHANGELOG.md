2e48559084464e6154ffe473e9ea4125558831b9
angle943 <justinkim943@gmail.com>
2021-10-28 20:38:45 -0500

    add unversioned-files

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# 0.1.0 (2015.5.27)
+
+#### Features
+
+- migrate to using electron-builder
---
+++ src/main/main.ts
@@ -0,0 +1,59 @@
+/* eslint global-require: off, no-console: off, @typescript-eslint/no-var-requires: off */
+
+/**
+ * This module executes inside of electron's main process. You can start
+ * electron renderer process from here and communicate with the other processes
+ * through IPC.
+ *
+ * When running `npm run build` or `npm run build:main`, this file is compiled to
+ * `./src/main.js` using webpack. This gives us some performance wins.
+ */
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import {app, ipcMain} from 'electron';
+import AppUpdater from './AppUpdater';
+import './ipcMain';
+import MainWindow from './MainWindow';
+import './Store';
+import {isDevelopment} from './util';
+
+export default AppUpdater;
+
+ipcMain.on('ipc-example', async (event, arg) => {
+  const msgTemplate = (pingPong: string) => `IPC test: ${pingPong}`;
+  console.log(msgTemplate(arg));
+  event.reply('ipc-example', msgTemplate('pong'));
+});
+
+if (process.env.NODE_ENV === 'production') {
+  const sourceMapSupport = require('source-map-support');
+  sourceMapSupport.install();
+}
+
+if (isDevelopment) {
+  require('electron-debug')();
+}
+
+/**
+ * Add event listeners...
+ */
+
+app.on('window-all-closed', () => {
+  // Respect the OSX convention of having the application in memory even
+  // after all windows have been closed
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app
+  .whenReady()
+  .then(() => {
+    MainWindow.createWindow();
+    app.on('activate', () => {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (!MainWindow.exists()) MainWindow.createWindow();
+    });
+  })
+  .catch(console.log);
